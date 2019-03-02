9b87a292e2e1e4702cf5c0ee53a4c28b36caf5b6
Calvin J Foster <cal.j.foster@gmail.com>
2019-03-02 14:12:20 -0600

    added to the js file

--- main.js
+++ main.js
@@ -38,3 +38,43 @@ console.log(nuggetizer('pig'));
 console.log(nuggetizer('fish'));
 console.log(nuggetizer('chicken'));
 console.log(nuggetizer('beef'));
+
+
+
+const dogBreed = (dogname) => {
+    return `my favorite dog breed is ${dogname}`
+}
+
+console.log(dogBreed('pitbull'));
+
+
+
+
+const dogBreedDiv = document.getElementById('dog-breeds');
+console.log('dogbreedDiv', dogBreedDiv);
+dogBreedDiv.innerHTML = dogBreed('pitbull');
+
+const nuggetizerDiv = document.getElementById('nuggets');
+console.log('nuggetizerDiv', nuggetizerDiv);
+nuggetizerDiv.innerHTML = nuggetizer('chicken');
+nuggetizerDiv.innerHTML += nuggetizer('deer');
+
+const printToDom = (divId, textToPrint) => {
+    const selectDiv = document.getElementById(divId);
+    selectDiv.innerHTML += textToPrint;
+};
+
+printToDom('nuggets', ' eewwwwww');
+printToDom('nuggets', nuggetizer('bear'));
+
+let bandNumber = 1;
+
+const addBand = (bandName) => {
+    const bandText = `<h5>${bandNumber}. ${bandName}</h5>`;
+    bandNumber =bandNumber + 1;
+    printToDom('band-list', bandText);
+    
+};
+
+addBand('Outkast');
+addBand('The Roots');
