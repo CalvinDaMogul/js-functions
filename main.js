afa9e6c814b9cdbe8384cf9e6da13069031d43ba
Calvin J Foster <cal.j.foster@gmail.com>
2019-03-02 11:07:43 -0600

    name printer function works

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log('hi');
+//Calvin Foster
+//JayZ
+//Prince
+//John Doe 
+
+
+// const firstname = 'Calvin';
+// const lastname = 'Foster';
+// console.log(firstname +' '+ lastname);
+// console.log(`${firstname} ${lastname}`);
+
+// const firstname1 = 'John';
+// const lastname1 = 'Doe';
+// console.log(`${firstname1} ${lastname1} `);
+
+// const firstname2 = 'J';
+// const lastname2 = 'Cole';
+// console.log(`${firstname2} ${lastname2}`);
+
+
+const namePrinter = (firstname, lastname) => {
+    console.log(`${firstname} ${lastname}`);
+};
+
+namePrinter('Calvin', 'Foster');
+namePrinter('Tobe', 'Nwigwe');
